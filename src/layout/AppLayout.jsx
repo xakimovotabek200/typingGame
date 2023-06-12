8b1a481126c695063f74390c88db29ea45eb9179
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-06-12 13:10:40 +0500

    game

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,12 +1,15 @@
 import Footer from "@/Components/Footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
+import { ThemeProvider } from "next-themes";
 
 const AppLayout = ({ children }) => {
   return (
     <div className=" bg-gray-50 container mx-auto">
-      <Navbar />
-      {children}
-      <Footer />
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Navbar />
+        {children}
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 };
